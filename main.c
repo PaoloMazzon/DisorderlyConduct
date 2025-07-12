f77b4080e8f86d968a438e7c5370e2a5f41e8074
Paolo Mazzon <pdmazzon@gmail.com>
2025-07-12 13:12:00 -0700

    Work on adding some basic character and physics stuff

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <oct/Octarine.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <math.h>
 
 ///////////////////////// ENUMS /////////////////////////
 typedef enum {
@@ -70,30 +71,41 @@ const Oct_Vec2 CHARACTER_HP_RANGES[] = {
 };
 
 const int32_t MAX_CHARACTERS = 100;
+const int32_t MAX_PROJECTILES = 100;
+const int32_t MAX_PARTICLES = 100;
+const float GROUND_FRICTION = 0.05;
+const float AIR_FRICTION = 0.01;
+const float GRAVITY = 0.5;
+const float BOUNCE_PRESERVED = 0.20; // how much velocity is preserved when rebounding off walls
 
 ///////////////////////// STRUCTS /////////////////////////
-typedef struct Character_t {
-    // Type of opp
-    CharacterType type;
-
-    // True if this is the current player false means AI
-    bool player_controlled;
-
-    // Physics
+typedef struct PhysicsObject_t {
     float x;
     float y;
     float x_vel;
     float y_vel;
 
-    // Only applies to ai, players get 1 tapped bozo
-    float hp;
+    // For collisions
+    bool noclip;
+    float bb_width;
+    float bb_height;
+} PhysicsObject;
 
-    // Only applies to player, as he will EXPLODE when timeout (enemies fling off-screen)
-    float lifespan;
+typedef struct Character_t {
+    CharacterType type; // Type of opp
+    Oct_SpriteInstance sprite;
+    bool player_controlled; // True if this is the current player false means AI
+    PhysicsObject physx;
+    float hp; // Only applies to ai, players get 1 tapped bozo
+    float lifespan; // Only applies to player, as he will EXPLODE when timeout (enemies fling off-screen)
+    bool alive;
 } Character;
 
 typedef struct Particle_t {
     bool sprite_based; // if true the sprite and frame is used
+    PhysicsObject physx;
+    float lifetime; // in seconds
+    _Atomic bool alive;
     union {
         struct {
             Oct_Sprite sprite;
@@ -101,26 +113,134 @@ typedef struct Particle_t {
         };
         Oct_Texture texture;
     };
-
-    // Physics
-    float x;
-    float y;
-    float x_vel;
-    float y_vel;
 } Particle;
 
+typedef struct Projectile_t {
+    PhysicsObject physx;
+    float lifetime; // in seconds
+    Oct_Texture tex;
+    float damage; // only relevant for enemies
+    _Atomic bool alive;
+} Projectile;
+
+typedef struct GameState_t {
+    Oct_Tilemap level_map;
+    Character characters[MAX_CHARACTERS];
+    Projectile projectiles[MAX_PROJECTILES];
+    Particle particles[MAX_PARTICLES];
+} GameState;
+
+GameState state;
+
 ///////////////////////// HELPERS /////////////////////////
+static inline float sign(float x) {
+    return x > 0 ? 1 : (x < 0 ? -1 : 0);
+}
+
+// checks for collisions against the tilemap
+bool collision_at(float x, float y, float width, float height) {
+    int32_t grid_x1 = floorf(x / oct_TilemapCellWidth(state.level_map));
+    int32_t grid_y1 = floorf(y / oct_TilemapCellHeight(state.level_map));
+    int32_t grid_x2 = floorf((x + width) / oct_TilemapCellWidth(state.level_map));
+    int32_t grid_y2 = floorf((y + height) / oct_TilemapCellHeight(state.level_map));
+
+    return oct_GetTilemap(state.level_map, grid_x1, grid_y1) != 0 ||
+           oct_GetTilemap(state.level_map, grid_x2, grid_y1) != 0 ||
+           oct_GetTilemap(state.level_map, grid_x1, grid_y2) != 0 ||
+           oct_GetTilemap(state.level_map, grid_x2, grid_y2) != 0;
+}
+
+void process_physics(PhysicsObject *physx, float x_acceleration, float y_acceleration) {
+    // Add acceleration to velocity
+    physx->x_vel += x_acceleration;
+    physx->y_vel += y_acceleration;
+
+    const bool kinda_touching_ground = collision_at(physx->x, physx->y + 1, physx->bb_width, physx->bb_height);
+
+    // Friction and gravity
+    if (kinda_touching_ground) {
+        physx->x_vel *= GROUND_FRICTION;
+    } else {
+        physx->x_vel *= AIR_FRICTION;
+    }
+    physx->y_vel + GRAVITY;
+
+    // TODO: Collisions with other entities
+
+    // Bouncy dogshit collisions
+    if (collision_at(physx->x + physx->x_vel, physx->y, physx->bb_width, physx->bb_height)) {
+        // Get close to the wall
+        while (!collision_at(physx->x + (sign(physx->x_vel) * 0.1), physx->y, physx->bb_width, physx->bb_height))
+            physx->x += (sign(physx->x_vel) * 0.1);
+
+        // Bounce off the wall slightly
+        physx->x_vel = physx->x_vel * (-BOUNCE_PRESERVED);
+    }
+    physx->x += physx->x_vel;
+    if (collision_at(physx->x, physx->y + physx->y_vel, physx->bb_width, physx->bb_height)) {
+        // Get close to the wall
+        while (!collision_at(physx->x, physx->y + (sign(physx->y_vel) * 0.1), physx->bb_width, physx->bb_height))
+            physx->x += (sign(physx->y_vel) * 0.1);
+
+        // Bounce off the wall slightly
+        physx->y_vel = physx->y_vel * (-BOUNCE_PRESERVED);
+    }
+    physx->y += physx->y_vel;
+}
+
+void draw_character(Character *character) {
+    if (character->type == CHARACTER_TYPE_JUMPER) {
+        oct_DrawSprite(
+                oct_GetAsset(gBundle, "sprites/jumper.json"),
+                &character->sprite,
+                (Oct_Vec2) {character->physx.x, character->physx.y}
+                );
+    } // TODO: The rest of these
+}
+
+void process_player(Character *character) {
+
+}
+
 void process_character(Character *character) {
+    // This is only to handle physics/acceleration
+    if (character->player_controlled) {
+        process_player(character);
+        return;
+    }
     // todo: this
 }
 
+void process_particle(Particle *particle) {
+    // todo: this
+}
+
+void process_projectile(Projectile *projectile) {
+    // todo: this
+}
+
+// copies a character into an available character slot and returns the character in the slot or
+// null if there was no available slot
+Character *add_character(Character *character) {
+    Character *slot = null;
+    for (int i = 0; i < MAX_CHARACTERS; i++) {
+        if (!state.characters[i].alive) {
+            slot = &state.characters[i];
+            memcpy(slot, character, sizeof(struct Character_t));
+            slot->alive = true;
+
+            // Handle sprite instance
+            if (slot->type == CHARACTER_TYPE_JUMPER) {
+                oct_InitSpriteInstance(&slot->sprite, oct_GetAsset(gBundle, "sprites/jumper.json"), true);
+            }  // TODO: The rest of these
+
+            break;
+        }
+    }
+    return slot;
+}
+
 ///////////////////////// GAME /////////////////////////
-typedef struct GameState_t {
-    Oct_Tilemap level_map;
-
-} GameState;
-GameState state;
-
 void game_begin() {
     memset(&state, 0, sizeof(struct GameState_t));
     state.level_map = oct_CreateTilemap(
@@ -134,6 +254,19 @@ void game_begin() {
             oct_SetTilemap(state.level_map, x, y, LEVEL_LAYOUT[(y * LEVEL_WIDTH) + x]);
         }
     }
+
+    // Add the player
+    add_character(&(Character){
+        .type = CHARACTER_TYPE_JUMPER,
+        .lifespan = 9999,
+        .player_controlled = true,
+        .physx = {
+                .x = 20,
+                .y = 20,
+                .bb_width = 8,
+                .bb_height = 8
+        }
+    });
 }
 
 GameStatus game_update() {
